c13ce6c1b1f3d487ac89c632b03be5df7bd6f6d7
matieuruth <matieu.ruth@gmail.com>
2020-04-12 21:42:35 -0400

    completed first event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+//event 1
+const mouseOver = document.querySelectorAll('a');
+mouseOver.forEach(anchor => anchor.addEventListener("mouseover", function(event)){
+  event.target.style.color = 'cyan';
+});
+
+//event 2
+const keyDown = document.querySelector('');
+
+//event 3
+const moveOver = document.querySelector('');
+const wheelItem = document.querySelector('');
+const dragItem = document.querySelector('');
+const loadItem = document.querySelector('');
+const resizeItem = document.querySelector('');
+const scrollItem = document.querySelector('');
+const selectItem = document.querySelector('');
+const clickItem = document.querySelector('');
